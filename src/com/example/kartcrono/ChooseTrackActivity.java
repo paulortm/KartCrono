4a774216fb09db79b7c35996acab53c9899e939a
paulortm <paulortmarques@gmail.com>
2014-08-24 15:04:56 +0100

    Added choose track activity

--- src/com/example/kartcrono/ChooseTrackActivity.java
+++ src/com/example/kartcrono/ChooseTrackActivity.java
@@ -1,25 +1,22 @@
 package com.example.kartcrono;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class ChooseTrackActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_choose_track);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.choose_track, menu);
 		return true;
 	}
 
@@ -33,11 +30,5 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-	
-	/** Called when the user clicks the Start button */
-	public void startChooseTrack(View view) {
-		Intent intent = new Intent(this, ChooseTrackActivity.class);
-	    startActivity(intent);
 	}
 }
