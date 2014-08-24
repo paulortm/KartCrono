10ccec4c8c55dcaea8aa2077e9c6105b39d3ba62
paulortm <paulortmarques@gmail.com>
2014-08-24 19:10:32 +0100

    Support for API 8

--- src/com/example/kartcrono/ChooseTrackActivity.java
+++ src/com/example/kartcrono/ChooseTrackActivity.java
@@ -1,16 +1,18 @@
 package com.example.kartcrono;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ChooseTrackActivity extends Activity {
+public class ChooseTrackActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_choose_track);
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
--- src/com/example/kartcrono/MainActivity.java
+++ src/com/example/kartcrono/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.kartcrono;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +33,10 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	/** Called when the user clicks the Start button */
 	public void startChooseTrack(View view) {
 		Intent intent = new Intent(this, ChooseTrackActivity.class);
-	    startActivity(intent);
+		startActivity(intent);
 	}
 }
